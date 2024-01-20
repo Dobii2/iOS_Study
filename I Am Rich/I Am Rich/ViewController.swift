e26e3c788e29ca36ebdec7b8f4c9eec24382786d
Dobii2 <vlsms119@naver.com>
2024-01-20 23:49:06 +0900

    Initial commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 송도협 on 1/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
